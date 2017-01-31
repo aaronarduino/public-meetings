a715f0a9d154ca3b1b68f8a4b44c05c6bd96c481
Aaron <aarona.arduino@gmail.com>
2017-01-30 22:43:37 -0600

    got context io working

---
+++ email/email.go
@@ -0,0 +1,72 @@
+package email
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+
+	contextio "github.com/dmlyons/goContextIO"
+)
+
+type Account struct {
+	client *contextio.ContextIO
+}
+
+type Messages []struct {
+	Date      int      `json:"date"`
+	Folders   []string `json:"folders"`
+	Addresses struct {
+		From struct {
+			Email string `json:"email"`
+			Name  string `json:"name"`
+		} `json:"from"`
+		To []struct {
+			Email string `json:"email"`
+		} `json:"to"`
+		ReplyTo []struct {
+			Email string `json:"email"`
+		} `json:"reply_to"`
+	} `json:"addresses"`
+	Sources []struct {
+		Label       string `json:"label"`
+		ResourceURL string `json:"resource_url"`
+	} `json:"sources"`
+	Subject        string `json:"subject"`
+	MessageID      string `json:"message_id"`
+	EmailMessageID string `json:"email_message_id"`
+	GmailMessageID string `json:"gmail_message_id"`
+	GmailThreadID  string `json:"gmail_thread_id"`
+	DateReceived   int    `json:"date_received"`
+	DateIndexed    int    `json:"date_indexed"`
+	ResourceURL    string `json:"resource_url"`
+}
+
+func NewAccount() *Account {
+	c := contextio.NewContextIO(os.Getenv("CONTEXT_KEY"), os.Getenv("CONTEXT_SECRET"))
+	return &Account{client: c}
+}
+
+func (a *Account) GetInbox() (Messages, error) {
+	reqbody := ""
+	resp, err := a.client.Do("GET", "2.0/accounts/588d7648e0637a16fa6e8067/messages", url.Values{}, &reqbody)
+	if err != nil {
+		log.Println(err)
+		return Messages{}, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	var msgs Messages
+	if err := json.Unmarshal(body, &msgs); err == io.EOF {
+		return msgs, nil
+	} else if err != nil {
+		log.Println(err)
+		return Messages{}, err
+	}
+	return msgs, nil
+}
--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/aaronarduino/public-meetings/email"
 
 	"github.com/gorilla/mux"
 )
 
 const port = ":8080"
 
+var from string
+
 func main() {
 	log.Println("Starting...")
 
+	acc := email.NewAccount()
+	msgs, err := acc.GetInbox()
+	if err != nil {
+		fmt.Println(err)
+	}
+	from = msgs[0].Addresses.From.Email
+	fmt.Println(msgs)
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index).Methods("GET")
-	router.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+	router.PathPrefix("/static").Handler(
+		http.StripPrefix("/static", http.FileServer(http.Dir("static"))),
+	)
 
 	log.Println("Started on port:", port)
 	log.Fatal(http.ListenAndServe(port, router))
@@ -25,5 +40,5 @@ func index(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
-	io.WriteString(w, "Hello, World!")
+	io.WriteString(w, "Hello, World! - "+from)
 }
